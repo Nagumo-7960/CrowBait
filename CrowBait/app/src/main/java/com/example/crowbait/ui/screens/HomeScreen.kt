812dc6f9507f5a688da7264c448f9c8b8739d2a7
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-18 14:20:35 +0900

    山札カードの作成

--- CrowBait/app/src/main/java/com/example/crowbait/ui/screens/HomeScreen.kt
+++ CrowBait/app/src/main/java/com/example/crowbait/ui/screens/HomeScreen.kt
@@ -1,11 +1,41 @@
 package com.example.crowbait.ui.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(){
-
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            ){
+        Card(
+            modifier = Modifier
+                .size(100.dp,200.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Deck",
+                    fontSize = 30.sp
+                )
+            }
+        }
+    }
 }
 
 @Preview
