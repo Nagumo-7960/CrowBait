5258176d896ee3ab2f314c33523e5ebc274f5c0e
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-19 12:26:16 +0900

    startDestinationの設定

---
+++ CrowBait/app/src/main/java/com/example/crowbait/Navigation.kt
@@ -0,0 +1,17 @@
+package com.example.crowbait
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.crowbait.ui.screens.HomeScreen
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen()
+        }
+    }
+
+}
