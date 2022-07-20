e7dd23990858ccd125450a4358bc0b4df7f11ee4
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-20 13:28:20 +0900

    対戦画面で何回戦かを表示

--- CrowBait/app/src/main/java/com/example/crowbait/ui/screens/BattleScreen.kt
+++ CrowBait/app/src/main/java/com/example/crowbait/ui/screens/BattleScreen.kt
@@ -1,15 +1,36 @@
 package com.example.crowbait.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun BattleScreen(){
+fun BattleScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier.padding(
+                top = 20.dp
+            )
+        ) {
+            Text(text = "1回戦", fontSize = 30.sp)
+        }
 
+    }
 }
 
 @Preview
 @Composable
-fun PreviewBattleScreen(){
+fun PreviewBattleScreen() {
     BattleScreen()
 }
