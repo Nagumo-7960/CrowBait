8ea4f8893454485b811fa340184e8a7ca0eb9982
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-21 15:09:12 +0900

    後攻プレイヤーの選択画面を作成

--- CrowBait/app/src/main/java/com/example/crowbait/ui/screens/HomeScreen.kt
+++ CrowBait/app/src/main/java/com/example/crowbait/ui/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.crowbait.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
@@ -64,6 +65,7 @@ fun HomeScreen(onClick: () -> Unit) {
 
     }
 }
+
 
 
 @Preview
---
+++ CrowBait/app/src/main/java/com/example/crowbait/ui/screens/SecondBattleScreen.kt
@@ -0,0 +1,97 @@
+package com.example.crowbait.ui.screens
+
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.crowbait.ui.components.DeckCard
+import com.example.crowbait.ui.components.SelectCard
+
+var secondPlayerHand: Array<Int?> = arrayOfNulls(15)
+var secondPlayerPoint = 0
+
+@Composable
+fun SecondBattleScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier.padding(
+                top = 20.dp
+            )
+        ) {
+            Text(text = "${battleRound}回戦", fontSize = 30.sp, color = Color.White)
+        }
+        Column(
+            modifier = Modifier.padding(
+                top = 20.dp
+            )
+        ) {
+            DeckCard(deckCardNumber = 1)
+        }
+        Column(modifier = Modifier.padding(top = 40.dp)) {
+            Row {
+                Card (){
+                    Text(modifier = Modifier.padding(start = 10.dp, end = 10.dp),text = "後攻プレイヤー", fontSize = 30.sp)
+                }
+                Row(modifier = Modifier.padding(start = 10.dp)) {
+                    Text(text = "得点：${secondPlayerPoint}", fontSize = 30.sp, color = Color.White)
+                }
+            }
+        }
+        Column(
+            modifier = Modifier
+                .padding(top = 40.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row {
+                for (i in 1..5) {
+                    Column(modifier = Modifier.padding(start = 10.dp)) {
+                        SelectCard(selectCardNumber = i, onClick = {getSecondPlayerHand(i)})
+                    }
+
+                }
+            }
+            Row(modifier = Modifier.padding(top = 20.dp)) {
+                for (i in 6..10) {
+                    Column(modifier = Modifier.padding(start = 10.dp)) {
+                        SelectCard(selectCardNumber = i, onClick = {getSecondPlayerHand(i)})
+                    }
+                }
+            }
+            Row(modifier = Modifier.padding(top = 20.dp)) {
+                for (i in 11..15) {
+                    Column(modifier = Modifier.padding(start = 10.dp)) {
+                        SelectCard(selectCardNumber = i, onClick = {getSecondPlayerHand(i)})
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+fun getSecondPlayerHand(handNumber: Int) {
+    //配列の(n回戦)番目に出す手の数字を入れる
+    secondPlayerHand[battleRound-1] = handNumber
+    Log.d("debag", "firstPlayerHand:${secondPlayerHand[battleRound-1]}")
+}
+
+@Preview
+@Composable
+fun PreviewSecondBattleScreen(){
+    SecondBattleScreen()
+}
