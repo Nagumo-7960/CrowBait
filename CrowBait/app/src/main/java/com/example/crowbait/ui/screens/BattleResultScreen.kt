dc8add4c317cb8422e34d6be439787488944e57c
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-24 13:38:45 +0900

    結果画面の作成途中

--- CrowBait/app/src/main/java/com/example/crowbait/ui/screens/BattleResultScreen.kt
+++ CrowBait/app/src/main/java/com/example/crowbait/ui/screens/BattleResultScreen.kt
@@ -1,11 +1,42 @@
 package com.example.crowbait.ui.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.crowbait.ui.components.DeckCard
 
 @Composable
 fun BattleResultScreen(){
-
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(
+            modifier = Modifier.padding(
+                top = 20.dp
+            )
+        ) {
+            Text(text = "${battleRound}回戦", fontSize = 30.sp, color = Color.White)
+        }
+        Column(
+            modifier = Modifier.padding(
+                top = 20.dp
+            )
+        ) {
+            DeckCard(deckCardNumber = 1)
+        }
+        Column(modifier = Modifier.padding(top = 20.dp)) {
+            Card(modifier = Modifier.size(300.dp,150.dp)) {
+                Box(contentAlignment = Alignment.Center){
+                    Text(text = "先攻プレイヤーの得点", fontSize = 30.sp)
+                }
+            }
+        }
+    }
 }
 
 @Preview
