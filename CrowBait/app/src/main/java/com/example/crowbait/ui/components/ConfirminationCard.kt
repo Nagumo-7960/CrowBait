75d98714239c41c5313caf71edb130f149f50350
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-30 23:27:06 +0900

    確認画面の作成

---
+++ CrowBait/app/src/main/java/com/example/crowbait/ui/components/ConfirminationCard.kt
@@ -0,0 +1,15 @@
+package com.example.crowbait.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ConfirminationCard(){
+
+}
+
+@Preview
+@Composable
+fun PreviewConfirminationCard(){
+    ConfirminationCard()
+}
