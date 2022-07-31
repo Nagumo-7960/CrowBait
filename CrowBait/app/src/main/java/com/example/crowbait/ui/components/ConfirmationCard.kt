ddcd63ac1481d9e6c52a8392e2919dcfa875d733
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-31 23:35:04 +0900

    確認カードのサイズと角丸を設定

--- CrowBait/app/src/main/java/com/example/crowbait/ui/components/ConfirmationCard.kt
+++ CrowBait/app/src/main/java/com/example/crowbait/ui/components/ConfirmationCard.kt
@@ -1,15 +1,22 @@
 package com.example.crowbait.ui.components
 
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun ConfirmationCard(){
+fun ConfirmationCard() {
+    Card(modifier = Modifier.size(400.dp, 200.dp), shape = RoundedCornerShape(20)) {
 
+    }
 }
 
 @Preview
 @Composable
-fun PreviewConfirmationCard(){
+fun PreviewConfirmationCard() {
     ConfirmationCard()
 }
