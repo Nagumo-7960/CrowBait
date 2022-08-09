bc6d2a9624955133b81116b450187dd872e8d053
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-08-09 22:43:50 +0900

    相手に渡すときの画面を作成

---
+++ CrowBait/app/src/main/java/com/example/crowbait/ui/screens/PassScreen.kt
@@ -0,0 +1,21 @@
+package com.example.crowbait.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun PassScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+
+    }
+}
+
+@Preview
+@Composable
+fun PreviewPassScreen(){
+    PassScreen()
+}
