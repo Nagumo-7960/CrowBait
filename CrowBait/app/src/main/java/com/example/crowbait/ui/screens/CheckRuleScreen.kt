55d4ab345d416bc62ed8f8b6a9cd7c974385fc69
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-08-24 23:22:01 +0900

    ルール設定画面の仮表記を設定

--- CrowBait/app/src/main/java/com/example/crowbait/ui/screens/CheckRuleScreen.kt
+++ CrowBait/app/src/main/java/com/example/crowbait/ui/screens/CheckRuleScreen.kt
@@ -1,4 +1,23 @@
 package com.example.crowbait.ui.screens
 
-class CheckRuleScreen {
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CheckRuleScreen(){
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+        Text(text = "ルール説明", color = Color.White)
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCheckRule(){
+    CheckRuleScreen()
 }
