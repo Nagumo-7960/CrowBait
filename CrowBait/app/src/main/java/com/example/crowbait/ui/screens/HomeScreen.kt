942a450ab6821236bcd142c9f2d29e63268ec8c0
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-09-16 23:27:18 +0900

    最終結果画面でクラスのフィールドを使う

--- CrowBait/app/src/main/java/com/example/crowbait/ui/screens/HomeScreen.kt
+++ CrowBait/app/src/main/java/com/example/crowbait/ui/screens/HomeScreen.kt
@@ -14,9 +14,9 @@ import androidx.navigation.compose.rememberNavController
 import com.example.crowbait.model.Player
 
 //test用
-//var deckNumberSet = arrayOf(-1, 1)
+var deckNumberSet = arrayOf(-1, 1)
 
-var deckNumberSet = arrayOf(-1,-2,-3,-4,-5,1,2,3,4,5,6,7,8,9,10)
+//var deckNumberSet = arrayOf(-1,-2,-3,-4,-5,1,2,3,4,5,6,7,8,9,10)
 var deckNumber = 1
 
 var firstPlayer = Player()
