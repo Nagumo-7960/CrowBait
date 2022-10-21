88e8b52f7d0e376205c6b335b034ca51e62ec3c5
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-10-21 23:36:15 +0900

    paddingの調整と文字色の変更

--- CrowBait/app/src/main/java/com/example/crowbait/ui/screens/CheckRuleScreen.kt
+++ CrowBait/app/src/main/java/com/example/crowbait/ui/screens/CheckRuleScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,7 +31,8 @@ fun CheckRuleScreen(toBack: () -> Unit) {
                 ) {
                     Text(
                         text = "もどる",
-                        fontSize = 15.sp
+                        fontSize = 15.sp,
+                        color = Color.White
                     )
                 }
             }
@@ -44,41 +46,49 @@ fun CheckRuleScreen(toBack: () -> Unit) {
                 Text(
                     text = "基本的なルール",
                     fontSize = 35.sp,
-                    fontWeight = FontWeight.Bold
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
                 )
                 Text(
                     text = "CrowBaitは、1~15の数字が書かれた手札から1枚のカードを選び、数字の大きい人が得点を獲得するカードゲームです。",
-                    fontSize = 20.sp
+                    fontSize = 20.sp,
+                    color = Color.White
                 )
             }
-            Column {
+            Column(modifier = Modifier.padding(top = 20.dp)) {
                 Text(
                     text = "ゲームの流れ",
                     fontSize = 35.sp,
-                    fontWeight = FontWeight.Bold
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
                 )
                 Text(
                     text = "-1~10までの数字のうち、1枚ずつ山札から選ばれる",
-                    fontSize = 20.sp
+                    fontSize = 20.sp,
+                    color = Color.White
                 )
                 Text(
                     text = "先行プレイヤーが手札を選ぶ→後攻プレイヤーが手札を選ぶ→対戦結果が出る",
-                    fontSize = 20.sp
+                    fontSize = 20.sp,
+                    color = Color.White
                 )
                 Text(
                     text = "これを手札の枚数15回繰り返し、最終的に得点が多かったプレイヤーの勝利",
-                    fontSize = 20.sp
+                    fontSize = 20.sp,
+                    color = Color.White
                 )
             }
-            Column {
+            Column(modifier = Modifier.padding(top = 20.dp)) {
                 Text(
                     text = "操作説明",
                     fontSize = 35.sp,
-                    fontWeight = FontWeight.Bold
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
                 )
                 Text(
                     text = "CrowBaitは、1~15の数字が書かれた手札から1枚のカードを選び、数字の大きい人が得点を獲得するカードゲームです。",
-                    fontSize = 20.sp
+                    fontSize = 20.sp,
+                    color = Color.White
                 )
             }
 
