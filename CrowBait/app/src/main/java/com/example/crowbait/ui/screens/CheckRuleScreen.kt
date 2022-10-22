98a26c7f8f3a41e69bc3304eb55b6311ccbe05f2
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-10-22 22:12:51 +0900

    操作説明を追加

--- CrowBait/app/src/main/java/com/example/crowbait/ui/screens/CheckRuleScreen.kt
+++ CrowBait/app/src/main/java/com/example/crowbait/ui/screens/CheckRuleScreen.kt
@@ -88,13 +88,139 @@ fun CheckRuleScreen(toBack: () -> Unit) {
                             fontWeight = FontWeight.Bold,
                             color = Color.White
                         )
-                        Text(
-                            text = "CrowBaitは、1~15の数字が書かれた手札から1枚のカードを選び、数字の大きい人が得点を獲得するカードゲームです。",
-                            fontSize = 20.sp,
-                            color = Color.White
-                        )
-                    }
+                        Column() {
+                            Text(
+                                text = "ホーム画面",
+                                fontSize = 20.sp,
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Column(modifier = Modifier.padding(start = 20.dp)) {
+                                Text(
+                                    text = "ルールブック",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "スタートボタン",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                            }
+                        }
+                        Column() {
+                            Text(
+                                text = "対戦画面",
+                                fontSize = 20.sp,
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Column(modifier = Modifier.padding(start = 20.dp)) {
+                                Text(
+                                    text = "手札",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "山札",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "何回戦かを表示",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "プレイヤーの得点",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "対戦中断ボタン",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                            }
+                        }
+                        Column() {
+                            Text(
+                                text = "1回戦ごとの結果画面",
+                                fontSize = 20.sp,
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Column(modifier = Modifier.padding(start = 20.dp)) {
+                                Text(
+                                    text = "何回戦かを表示",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "山札",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "先攻プレイヤーの選んだ手札",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "先攻プレイヤーの得点",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "後攻プレイヤーの選んだ手札",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "後攻プレイヤーの得点",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "次の対戦へ進むボタン",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                            }
+                        }
 
+                        Column() {
+                            Text(
+                                text = "最終結果画面",
+                                fontSize = 20.sp,
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Column(modifier = Modifier.padding(start = 20.dp)) {
+                                Text(
+                                    text = "勝者の表示",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "先攻プレイヤーの得点を表示",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "後攻プレイヤーの得点を表示",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                                Text(
+                                    text = "ホーム画面へ戻る",
+                                    fontSize = 20.sp,
+                                    color = Color.White
+                                )
+                            }
+                        }
+
+                    }
                 }
             }
         }
