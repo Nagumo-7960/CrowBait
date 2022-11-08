e7f33d6f70293b7263e64465fe4bd6a02aac38fe
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-11-08 22:32:05 +0900

    半透明黒の背景色を追加

--- CrowBait/app/src/main/java/com/example/crowbait/ui/components/ConfirmationCard.kt
+++ CrowBait/app/src/main/java/com/example/crowbait/ui/components/ConfirmationCard.kt
@@ -1,5 +1,6 @@
 package com.example.crowbait.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,44 +22,64 @@ fun HandConfirmationCard(
     cancel_button: () -> Unit,
     handCardNumber: Int?
 ) {
-    Card(modifier = Modifier.size(370.dp, 200.dp), shape = RoundedCornerShape(5)) {
-        Column(
-            modifier = Modifier.padding(top = 20.dp),
-        ) {
-            Column(modifier = Modifier.padding(top = 10.dp, start = 20.dp, end = 20.dp, bottom = 20.dp)) {
-                Text(text = "手札確認", fontSize = 30.sp, fontWeight = FontWeight.Bold)
-                Column(modifier = Modifier.padding(top = 10.dp)) {
-                    Text(text = "${handCardNumber}を選択してもよろしいですか？", fontSize = 20.sp)
-                }
-            }
-
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0x80000000)),
+        contentAlignment = Alignment.Center
+    ) {
+        Card(modifier = Modifier.size(370.dp, 200.dp), shape = RoundedCornerShape(5)) {
             Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(end = 30.dp, bottom = 30.dp)
-                    .wrapContentWidth(Alignment.End)
-                    .wrapContentHeight(Alignment.Bottom)
-                ) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Column(modifier = Modifier.padding(end = 20.dp)) {
-                        Text(
-                            text = "キャンセル",
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color.Red
-                        )
-                    }
-                    Column() {
-                        Text(
-                            text = "決定",
-                            fontSize = 20.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color.Red
-                        )
+                modifier = Modifier.padding(top = 20.dp),
+            ) {
+                Column(modifier = Modifier.padding(top = 10.dp, start = 20.dp, end = 20.dp, bottom = 20.dp)) {
+                    Text(text = "手札確認", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+                    Column(modifier = Modifier.padding(top = 10.dp)) {
+                        Text(text = "${handCardNumber}を選択してもよろしいですか？", fontSize = 20.sp)
                     }
                 }
-            }
 
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(end = 30.dp, bottom = 30.dp)
+                        .wrapContentWidth(Alignment.End)
+                        .wrapContentHeight(Alignment.Bottom)
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Column(
+                            modifier = Modifier
+                                .padding(end = 20.dp)
+                                .clickable(
+                                    enabled = true,
+                                    onClick = cancel_button
+                                )
+                        ) {
+                            Text(
+                                text = "キャンセル",
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Red
+                            )
+                        }
+                        Column(
+                            modifier = Modifier
+                                .clickable(
+                                    enabled = true,
+                                    onClick = determine_button
+                                )
+                        ) {
+                            Text(
+                                text = "決定",
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Red
+                            )
+                        }
+                    }
+                }
+
+            }
         }
     }
 }
